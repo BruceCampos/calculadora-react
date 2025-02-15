43d7ab379f12ea68bad454667ec4cec5348f52d7
Bruce Campos <brucecamposnh@gmail.com>
2025-02-15 17:23:09 -0300

    Calculadora React

---
+++ src/App.js
@@ -0,0 +1,134 @@
+import Input from "./components/Input";
+import Button from "./components/Button";
+
+import  { Container, Content, Row } from "./styles";
+import { useState } from "react";
+
+
+const App = () => {
+  const [currentNumber, setCurrentNumber] = useState("0");
+  const [firstNumber, setFirstNumber] = useState("0");
+  const [operation, setOperation] = useState("");
+
+  const handleOnClear = () => {
+    setCurrentNumber("0");
+    setFirstNumber("0");
+    setOperation("");
+  }
+
+  const handleAddNumber = (number) => {
+    setCurrentNumber(prev => `${prev === "0" ? "" : prev}${number}`);
+  }
+
+  // Function to sum the numbers
+  const handleSumNumbers = () => {
+
+    if (firstNumber === "0") {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber("0");
+      setOperation("+");
+    } else {
+      const sum = Number(firstNumber) + Number(currentNumber);
+      setCurrentNumber(String(sum));
+    }
+  }
+
+  // Function to subtract the numbers
+  const handleSubtractNumbers = () => {
+
+    if (firstNumber === "0") {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber("0");
+      setOperation("-");
+    } else {
+      const subtract = Number(firstNumber) - Number(currentNumber);
+      setCurrentNumber(String(subtract));
+    }
+  }
+
+  // function to multiply the numbers
+  const handleMultiplyNumbers = () => {
+
+    if (firstNumber === "0") {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber("0");
+      setOperation("x");
+    } else {
+      const multiply = Number(firstNumber) * Number(currentNumber);
+      setCurrentNumber(String(multiply));
+    }
+  }
+
+  // Function to divide the numbers
+  const handleDivideNumbers = () => {
+    if (firstNumber === "0") {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber("0");
+      setOperation("÷");
+    } else {
+      const divide = Number(firstNumber) / Number(currentNumber);
+      setCurrentNumber(String(divide));
+    }
+  }
+
+  // Function to calculate the result
+  const handleEquals = () => {
+
+    if (firstNumber !== "0" && operation !== "" && currentNumber !== "0") {
+      switch (operation) {
+        case "+":
+          handleSumNumbers();
+          setOperation("");
+          break;
+        case "-":
+          handleSubtractNumbers();
+          setOperation("");
+          break;
+        case "x":
+          handleMultiplyNumbers();
+          setOperation("");
+          break;
+        case "÷":
+          handleDivideNumbers();
+          setOperation("");
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  return (
+    <Container>
+      <Content>
+        <Input value={currentNumber} />
+        <Row>
+          <Button label="0" onClick={() => handleAddNumber('0')} />
+          <Button label="C" onClick={handleOnClear} />
+          <Button label="=" onClick={handleEquals} />
+          <Button label="÷" onClick={handleDivideNumbers} />
+        </Row>
+        <Row>
+          <Button label="7" onClick={() => handleAddNumber('7')} />
+          <Button label="8" onClick={() => handleAddNumber('8')} />
+          <Button label="9" onClick={() => handleAddNumber('9')} />
+          <Button label="×" onClick={handleMultiplyNumbers} />
+        </Row>
+        <Row>
+          <Button label="4" onClick={() => handleAddNumber('4')} />
+          <Button label="5" onClick={() => handleAddNumber('5')} />
+          <Button label="6" onClick={() => handleAddNumber('6')} />
+          <Button label="-" onClick={handleSubtractNumbers} />
+        </Row>
+        <Row>
+          <Button label="1" onClick={() => handleAddNumber('1')} />
+          <Button label="2" onClick={() => handleAddNumber('2')} />
+          <Button label="3" onClick={() => handleAddNumber('3')} />
+          <Button label="+" onClick={handleSumNumbers} />
+        </Row>
+      </Content>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/Input/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #00aa00;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto', sans-serif;
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #00AAcc;
+        border: 0;
+        text-align: right;
+        padding: 0 20px;
+        font-size: 24px;
+        font-family: 'Roboto', sans-serif;
+        color: #FFF;
+        }
+`
